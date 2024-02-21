995138c32c425b63667a35dba1eb8f2c9c55d274
Duygu <duyguasenakaya@gmail.com>
2024-02-21 17:45:19 +0300

    add save customer method

---
+++ src/main/java/com/sisterslab/sisterslabcustomer/service/CustomerService.java
@@ -0,0 +1,18 @@
+package com.sisterslab.sisterslabcustomer.service;
+
+import com.sisterslab.sisterslabcustomer.model.Customer;
+import com.sisterslab.sisterslabcustomer.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    public Customer saveCustomer(Customer customer){
+        return  customerRepository.save(customer);
+
+    }
+}
