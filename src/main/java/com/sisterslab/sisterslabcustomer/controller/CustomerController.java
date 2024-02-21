f90da2f0ff0dcce8c5b836cc9eb9da6b5cd4d41d
Duygu <duyguasenakaya@gmail.com>
2024-02-21 18:33:00 +0300

    add get customers method

--- src/main/java/com/sisterslab/sisterslabcustomer/controller/CustomerController.java
+++ src/main/java/com/sisterslab/sisterslabcustomer/controller/CustomerController.java
@@ -5,10 +5,9 @@ package com.sisterslab.sisterslabcustomer.controller;
 import com.sisterslab.sisterslabcustomer.model.Customer;
 import com.sisterslab.sisterslabcustomer.service.CustomerService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/customers")
@@ -18,9 +17,19 @@ public class CustomerController {
 
     @PostMapping()
     public Customer addCustomer(@RequestBody Customer customer){
-
         return  customerService.saveCustomer(customer);
     }
+
+    @GetMapping()
+    public List<Customer> allCustomer(){
+        return customerService.allCustomer();
+    }
+
+    @GetMapping("{id}")
+    public Customer getCustomerById(@PathVariable("id")Long id){
+        return  customerService.getCustomerFingById(id);
+    }
+
 
 
 }
--- src/main/java/com/sisterslab/sisterslabcustomer/service/CustomerService.java
+++ src/main/java/com/sisterslab/sisterslabcustomer/service/CustomerService.java
@@ -3,10 +3,15 @@ package com.sisterslab.sisterslabcustomer.service;
 import com.sisterslab.sisterslabcustomer.model.Customer;
 import com.sisterslab.sisterslabcustomer.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j  //log basmamızı sağlar
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
@@ -14,5 +19,19 @@ public class CustomerService {
     public Customer saveCustomer(Customer customer){
         return  customerRepository.save(customer);
 
+    }
+
+    public List<Customer> allCustomer() {
+        return customerRepository.findAll();
+    }
+
+    public Customer getCustomerFingById(Long id) {
+        Optional<Customer> customer=customerRepository.findById(id);
+        if(customer.isPresent()){
+            return  customer.get();
+        }else {
+            log.info("Aradığınız id de kullanıcı yok!!");
+            return  null;
+        }
     }
 }
